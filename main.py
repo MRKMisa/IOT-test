32f412a9d700f4c017ec1e640ed53dff0d62d856
MRKMisa <michaelluky08@gmail.com>
2026-05-21 10:26:36 +0200

    Led script

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("main")
+from gpiozero import LED
+import time
+
+led1 = LED(15)
+
+while True:
+    print(f"ON \r", end="")
+    led1.on()
+
+    time.sleep(1)
+    led1.off()
+    print(f"OFF\r", end="")
+
+    time.sleep(1)
